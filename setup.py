3f1e59b65f36d631b5936fc05afc5c50c17932bc
Nickolai Novik <nickolainovik@gmail.com>
2016-12-07 22:07:16 +0200

    add base files

---
+++ setup.py
@@ -0,0 +1,61 @@
+import os
+import re
+import sys
+from setuptools import setup, find_packages
+
+
+PY_VER = sys.version_info
+
+if not PY_VER >= (3, 5):
+    raise RuntimeError("aiomonitor doesn't support Python earlier than 3.5")
+
+
+def read(f):
+    return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
+
+
+install_requires = ['janus',
+                    'aioconsole']
+extras_require = {}
+
+
+def read_version():
+    regexp = re.compile(r"^__version__\W*=\W*'([\d.abrc]+)'")
+    init_py = os.path.join(os.path.dirname(__file__),
+                           'aiomonitor', '__init__.py')
+    with open(init_py) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match is not None:
+                return match.group(1)
+        else:
+            msg = 'Cannot find version in aiomonitor/__init__.py'
+            raise RuntimeError(msg)
+
+
+classifiers = [
+    'License :: OSI Approved :: MIT License',
+    'Intended Audience :: Developers',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Operating System :: POSIX',
+    'Environment :: Web Environment',
+    'Development Status :: 3 - Alpha',
+]
+
+
+setup(name='aiohttp-admin',
+      version=read_version(),
+      description=('aiomonitor'),
+      long_description='\n\n'.join((read('README.rst'), read('CHANGES.txt'))),
+      classifiers=classifiers,
+      platforms=['POSIX'],
+      author="Nikolay Novik",
+      author_email="nickolainovik@gmail.com",
+      url='https://github.com/jettify/aiomonitor',
+      download_url='https://github.com/jettify/aiomonitor',
+      license='Apache 2',
+      packages=find_packages(),
+      install_requires=install_requires,
+      extras_require=extras_require,
+      include_package_data=True)
