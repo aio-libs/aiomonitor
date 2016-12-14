f341213182dd0e08db0ece82c698f0284602701f
Nickolai Novik <nickolainovik@gmail.com>
2016-12-14 21:54:27 +0200

    bump version

--- aiomonitor/__init__.py
+++ aiomonitor/__init__.py
@@ -26,4 +26,4 @@ from .monitor import (Monitor, start_monitor,
 
 __all__ = ('Monitor', 'start_monitor', 'MONITOR_HOST', 'MONITOR_PORT',
            'CONSOLE_PORT')
-__version__ = '0.0.4a0'
+__version__ = '0.1.0'
