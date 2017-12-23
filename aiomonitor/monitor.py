3f1c7cb4fdd0f6ce24cfe6f7cea5443608e79dc3
Nikolay Novik <nickolainovik@gmail.com>
2017-12-23 16:09:27 +0200

    user py3.5 syntax

--- aiomonitor/monitor.py
+++ aiomonitor/monitor.py
@@ -59,7 +59,7 @@ class Monitor:
         self._closing = threading.Event()
         self._closed = False
         self._started = False
-        self._console_future: Optional[Future[Any]] = None
+        self._console_future = None  # type: Optional[Future[Any]]
 
     def __repr__(self) -> str:
         name = self.__class__.__name__
