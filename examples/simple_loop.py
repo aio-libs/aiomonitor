aab9bd97dc172e29aba8c898e047806853bd03c8
Joongi Kim <me@daybreaker.info>
2022-09-26 17:50:03 +0900

    Update simple_loop example

--- examples/simple_loop.py
+++ examples/simple_loop.py
@@ -3,6 +3,9 @@ import asyncio
 import aiomonitor
 
 loop = asyncio.get_event_loop()
-with aiomonitor.start_monitor(loop=loop):
-    print("Now you can connect with: nc localhost 50101")
+with aiomonitor.start_monitor(loop=loop) as monitor:
+    print("Now you can connect with:")
+    print(f"  python -m aiomonitor.cli -H {monitor.host} -p {monitor.port}")
+    print("or")
+    print(f"  telnet {monitor.host} {monitor.port}  # Linux only")
     loop.run_forever()
