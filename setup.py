30a4faa591a14d1a94892cc1ac279cf5b2b12b55
Joongi Kim <me@daybreaker.info>
2023-04-24 11:04:01 -0600

    restyle setup.py a little

--- setup.py
+++ setup.py
@@ -6,8 +6,10 @@ def read(f):
     return open(os.path.join(os.path.dirname(__file__), f)).read().strip()
 
 
-install_requires = ['terminaltables',
-                    'aioconsole']
+install_requires = [
+    'terminaltables',
+    'aioconsole',
+]
 extras_require = {}
 
 
@@ -25,22 +27,23 @@ classifiers = [
 ]
 
 
-setup(name='aiomonitor',
-      use_scm_version=True,
-      setup_requires=['setuptools_scm'],
-      description=('aiomonitor adds monitor and python REPL '
-                   'capabilities for asyncio application'),
-      long_description='\n\n'.join((read('README.rst'), read('CHANGES.txt'))),
-      classifiers=classifiers,
-      platforms=['POSIX'],
-      author='Nikolay Novik',
-      author_email='nickolainovik@gmail.com',
-      url='https://github.com/aio-libs/aiomonitor',
-      download_url='https://pypi.python.org/pypi/aiomonitor',
-      license='Apache 2',
-      packages=find_packages(),
-      install_requires=install_requires,
-      extras_require=extras_require,
-      include_package_data=True,
-      python_requires=">=3.8",
-      )
+setup(
+    name='aiomonitor',
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    description=('aiomonitor adds monitor and python REPL '
+                 'capabilities for asyncio application'),
+    long_description='\n\n'.join((read('README.rst'), read('CHANGES.txt'))),
+    classifiers=classifiers,
+    platforms=['POSIX'],
+    author='Nikolay Novik',
+    author_email='nickolainovik@gmail.com',
+    url='https://github.com/aio-libs/aiomonitor',
+    download_url='https://pypi.python.org/pypi/aiomonitor',
+    license='Apache 2',
+    packages=find_packages(),
+    install_requires=install_requires,
+    extras_require=extras_require,
+    include_package_data=True,
+    python_requires=">=3.8",
+)
