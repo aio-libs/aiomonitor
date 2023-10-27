633bd181bf915eeaed782f396084a1b2c8a56dea
Joongi Kim <me@daybreaker.info>
2023-10-27 11:38:42 +0900

    Add more cmdarg options to demo.py

--- examples/demo.py
+++ examples/demo.py
@@ -34,12 +34,12 @@ async def inner1(tg: asyncio.TaskGroup) -> None:
 @preserve_termination_log
 async def timer_loop() -> None:
     async def interval_work():
-        await asyncio.sleep(1.5)
+        await asyncio.sleep(3.0)
 
     scope: weakref.WeakSet[asyncio.Task] = weakref.WeakSet()
     try:
         while True:
-            await asyncio.sleep(2.0)
+            await asyncio.sleep(3.0)
             t = asyncio.create_task(interval_work(), name="interval-worker")
             scope.add(t)
     except asyncio.CancelledError:
@@ -58,31 +58,65 @@ async def simple(request: web.Request) -> web.Response:
     return web.Response(text="Simple answer")
 
 
-async def main() -> None:
+async def main(
+    *,
+    history_limit: int,
+    num_timers: int,
+) -> None:
     loop = asyncio.get_running_loop()
-    with aiomonitor.start_monitor(loop, hook_task_factory=True):
+    log_timer_loop = logging.getLogger("demo.timer_loop")
+    log_aiohttp_server = logging.getLogger("demo.aiohttp_server")
+    with aiomonitor.start_monitor(
+        loop,
+        hook_task_factory=True,
+        max_termination_history=history_limit,
+    ):
         app = web.Application()
         app.router.add_get("/", simple)
-        app["timer_loop_task"] = asyncio.create_task(timer_loop())
+        timer_loop_tasks = set()
+        for idx in range(num_timers):
+            timer_loop_tasks.add(
+                asyncio.create_task(timer_loop(), name=f"timer-loop-{idx}")
+            )
+            log_timer_loop.info("started a timer loop (%d)", idx)
+        app["timer_loop_tasks"] = timer_loop_tasks
 
         async def _shutdown_timer_loop_task(app: web.Application) -> None:
-            t = app["timer_loop_task"]
-            t.cancel()
-            await t
+            for idx, t in enumerate(app["timer_loop_tasks"]):
+                log_timer_loop.info("shutting down a timer loop (%d)", idx)
+                t.cancel()
+                await t
 
         app.on_cleanup.append(_shutdown_timer_loop_task)
+        log_aiohttp_server.info(
+            "started a sample aiohttp server: http://localhost:8090/"
+        )
         await web._run_app(app, port=8090, host="localhost")
 
 
 if __name__ == "__main__":
     logging.basicConfig()
     logging.getLogger("aiomonitor").setLevel(logging.DEBUG)
+    logging.getLogger("demo").setLevel(logging.DEBUG)
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--loop",
         type=LoopImpl,
         choices=list(LoopImpl),
-        help="The event loop implementation to use",
+        default=LoopImpl.ASYNCIO,
+        help="The event loop implementation to use [default: asyncio]",
+    )
+    parser.add_argument(
+        "--history-limit",
+        type=int,
+        default=10,
+        help="Set the maximum number of task termination history [default: 10]",
+    )
+    parser.add_argument(
+        "--num-timers",
+        type=int,
+        default=5,
+        help="Set the number of timer loop tasks to demonstrate persistent termination logs",
     )
     args = parser.parse_args()
     match args.loop:
@@ -91,6 +125,11 @@ if __name__ == "__main__":
         case _:
             pass
     try:
-        asyncio.run(main())
+        asyncio.run(
+            main(
+                history_limit=args.history_limit,
+                num_timers=args.num_timers,
+            )
+        )
     except KeyboardInterrupt:
         pass
