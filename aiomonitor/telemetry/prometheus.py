cd9932f7e9cc7a1abaffcef35706760d86320662
Joongi Kim <me@daybreaker.info>
2024-04-13 19:18:38 +0900

    Add pid and explicit content type to the prometheus output

--- aiomonitor/telemetry/prometheus.py
+++ aiomonitor/telemetry/prometheus.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import asyncio
 import io
+import os
 import time
 from typing import TYPE_CHECKING
 
@@ -16,7 +17,10 @@ async def prometheus_exporter(request: web.Request) -> web.Response:
     out = io.StringIO()
     print("# TYPE asyncio_tasks gauge", file=out)
     now = time.time_ns() // 1000  # unix timestamp in msec
+    pid = os.getpid()  # we use threads to run the aiomonitor UI thread, so the pid is same to the monitored program.
     all_task_count = len(asyncio.all_tasks(ctx.monitor._monitored_loop))
-    print(f"asyncio_running_tasks {all_task_count} {now}", file=out)
+    print(
+        'asyncio_running_tasks{pid="%d"} %d %d' % (pid, all_task_count, now), file=out
+    )
     # TODO: count per name of explicitly named tasks (using labels)
-    return web.Response(body=out.getvalue())
+    return web.Response(body=out.getvalue(), content_type="text/plain")
